80a45712f279c128ddddcef731bf500f030a2963
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2023-08-02 04:27:46 +0300

    dd

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Controller\Api\AuthController;
+use App\Http\Controllers\Api\AuthController;
 
 
 /*
