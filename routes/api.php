b2f93acec3da2188ac06985e3692b3b6e64514e3
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2023-08-02 03:50:53 +0300

    newupdates

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Controller\UserController;
+use App\Http\Controller\UserController;
 use App\Controller\Api\AuthController;
 
 
